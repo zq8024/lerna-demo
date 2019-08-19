dc76f05371bb3a1ff72f38422835b8093f56a11d
zq <zq@aa.com>
2019-08-19 15:05:15 +0800

    add jquery

--- packages/h5/src/index.ts
+++ packages/h5/src/index.ts
@@ -1,5 +1,6 @@
 import { sayHello, getStatusName, Teacher } from "@agp/web";
 import { Status } from '@agp/core';
+import $ from 'jquery';
 
 sayHello();
 
@@ -9,6 +10,8 @@ console.log(s);
 let te = new Teacher('jim');
 te.show();
 te.book();
+
+$('body').append($('<div>test</div>'))
 
 
 
